c6c7893d97dbce96a80d5edc722b24d526e2182f
Maratik <92305667+Goryhon@users.noreply.github.com>
2024-01-28 15:26:44 +1000

    Delete CodeGenerator.py

--- CodeGenerator.py
+++
@@ -1,6 +0,0 @@
-class CodeGenerator:
-    def __init__(self, source):
-        self.source = source
-
-    def __repr__(self) -> str:
-        return self.source.headerProgram + "\n{\n" + self.source.getGeneratedText() + "}"
